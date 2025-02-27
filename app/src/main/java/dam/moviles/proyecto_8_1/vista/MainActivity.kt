f0db4e6161aa8cde638e1b23b3bc4665981afa84
DFran49 <francresmartin@gmail.com>
2025-02-27 15:55:38 +0100

    ad

--- app/src/main/java/dam/moviles/proyecto_8_1/vista/MainActivity.kt
+++ app/src/main/java/dam/moviles/proyecto_8_1/vista/MainActivity.kt
@@ -1,6 +1,7 @@
 package dam.moviles.proyecto_8_1.vista
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dam.moviles.proyecto_8_1.R
 import dam.moviles.proyecto_8_1.databinding.ActivityMainBinding
